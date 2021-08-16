7b1aa6d403b9191b580671257fda7fe0de01210f
selinamelia <sellinaamelia@gmail.com>
2021-08-16 23:43:09 +0700

    change seeder rb

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -3,7 +3,8 @@ pages << {
   method: "GET",
   fetch_type: "browser",
   url: "https://www.aliexpress.com/category/100003109/women-clothing.html",
+  headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
   vars: {
-      category: "Woman's Clothing"
+    category: "Women's clothing"
   }
 }
