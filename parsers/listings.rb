9256b2c7007f1ac4a09f35826ed5752142cbc042
selinamelia <sellinaamelia@gmail.com>
2021-08-18 00:41:16 +0700

    update listings rb for pagination link

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -22,14 +22,14 @@ end
 
 
 # load paginated links
-# pagination_links = nokogiri.at_css('div.next-pagination-list')
-# pagination_links.each do |link|
-#     l_val = link.text.strip
-#     if l_val !~ /next|previous/i && l_val.to_i < 8 #limit pagination to 7 pages
-#         url = URI.join('https://www.aliexpress.com/category/100003109/women-clothing.html?page=', l_val)
-#         pages << {
-#             url: url,
-#             page_type: 'listings'
-#         }
-#     end
-# end
+pagination_links = nokogiri.at_css('button.next-btn next-medium next-btn-normal next-pagination-item next-current')
+pagination_links.each do |link|
+    l_val = link.text.strip
+    if l_val !~ /next|previous/i && l_val.to_i < 8 #limit pagination to 7 pages
+        url = URI.join('https://www.aliexpress.com/category/100003109/women-clothing.html?page=', l_val)
+        pages << {
+            url: url,
+            page_type: 'listings'
+        }
+    end
+end
