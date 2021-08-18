39366d7cfb0717ce5220616906cdf46d8f3b93fe
selinamelia <sellinaamelia@gmail.com>
2021-08-18 23:38:31 +0700

    update paginated links

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -3,6 +3,7 @@ nokogiri = Nokogiri.HTML(content)
 products = nokogiri.css('div.JIIxO > div._1OUGS')
 products.each do |product|
     a_element = product.at_css('a.awV9E')
+    name = product.at_css('a.awV9E > span')&.text
     if a_element
         url = URI.join('https://www.aliexpress.com', a_element['href']).to_s.split('?').first
         if url =~ /\Ahttps?:\/\//i
@@ -13,7 +14,8 @@ products.each do |product|
                 force_fetch: true,
                 vars: {
                     category: page['vars']['category'],
-                    url: url
+                    url: url,
+                    name: name
                 }
             }
         end
@@ -22,14 +24,26 @@ end
 
 
 # load paginated links
-pagination_links = nokogiri.css('button.next-btn next-medium next-btn-normal next-pagination-item next-current')
-pagination_links.each do |link|
-    l_val = link.text.strip
-    if l_val !~ /next|previous/i && l_val.to_i < 8 #limit pagination to 7 pages
-        url = URI.join('https://www.aliexpress.com/category/100003109/women-clothing.html?page=', l_val).to_s
+current_page = nokogiri.at_css('button.next-current')&.text.to_i
+if current_page == 1
+    max_page_tag = nokogiri.at_css('span.total-page')&.text
+    max_page = max_page_tag.split(' ')[1].to_i
+
+    for i in 2..max_page
+        next_page_url = "https://www.aliexpress.com/category/100003109/women-clothing.html?page=" + i
         pages << {
-            url: url,
-            page_type: 'listings'
+            page_type: 'listings',
+            url: next_page_url,
+            method: "GET",
+            force_fetch: true,
+            fetch_type: "browser",
+            vars: {
+                page_num: 1
+            }
         }
+
+        if i >= max_listing_page_to_scrape
+            break 
+        end
     end
 end
