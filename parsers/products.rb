d7fab53c5c1bbc8d943c1b847070c3ff4d724a5c
selinamelia <sellinaamelia@gmail.com>
2021-08-20 00:22:20 +0700

    update products rb line 33

--- parsers/products.rb
+++ parsers/products.rb
@@ -19,19 +19,6 @@ product['image_url'] = nokogiri.at_css('img.magnifier-image')['src']
 #extract discount 
 product['discount'] = nokogiri.at_css('span.product-price-mark').text
 
-# if discount_element
-#     discount_low_price = discount_element.css('span').find{|span| span['itemprop'] == 'lowPrice' }
-#     if discount_low_price
-#         product['discount_low_price'] = discount_element.css('span').find{|span| span['itemprop'] == 'lowPrice' }.text.to_f
-#         product['discount_high_price'] = discount_element.css('span').find{|span| span['itemprop'] == 'highPrice' }.text.to_f
-#     else
-#         product['discount_price'] = discount_element.text.to_f
-#     end
-# end
-
-
-product['original_price'] = nokogiri.at_css('span.product-price-del span.product-price-value').text.strip
-
 price_element = nokogiri.css('.product-price')
 if price_element
     #extract discounted price
@@ -40,13 +27,6 @@ if price_element
     #extract original price
     product['original_price'] = price_element.at_css('span.product-price-del span').text
 end
-
-
-#extract categories
-# breadcrumb_categories = nokogiri.at_css('.ui-breadcrumb').text.strip
-# categories = breadcrumb_categories.split('>').map{|category| category.strip }
-# categories.delete('Home')
-# product['categories'] = categories
 
 #extract SKUs
 skus_element = nokogiri.css('.product-quantity-tip')
@@ -63,10 +43,7 @@ product['rating'] = nokogiri.at_css('div.positive-fdbk')[1].text
 
 #extract orders count
 product['orders_count'] = nokogiri.at_css('span.product-reviewer-sold').text.strip.split(' ').first.to_i
-# order_count_element = nokogiri.at_css('#j-order-num')
-# if order_count_element
-#     product['orders_count'] = order_count_element.text.strip.split(' ').first.to_i
-# end
+
 
 # #extract shipping info
 # shipping_element = nokogiri.at_css('dl#j-product-shipping')
